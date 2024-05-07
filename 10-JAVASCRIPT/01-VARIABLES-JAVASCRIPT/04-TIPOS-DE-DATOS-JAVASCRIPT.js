b0ce13a88ae88aa9a50423da483138deefbbe4eb
Jose Ramos <jlramos5@misena.edu.co>
2024-05-07 01:45:00 -0500

    V7-Algoritmia

---
+++ 10-JAVASCRIPT/01-VARIABLES-JAVASCRIPT/04-TIPOS-DE-DATOS-JAVASCRIPT.js
@@ -0,0 +1,18 @@
+// Variable tipo entero
+let miEntero = 10
+console.log(miEntero)
+
+// Variable tipo flotante
+let miFlotante = 7.9
+console.log(miFlotante)
+
+// Variable tipo string
+var miCadena = "Hola"
+console.log(miCadena)
+
+// Variable tipo boolean
+let miBoolean = true
+console.log(miBoolean)
+
+let miNull = null
+console.log(miNull)
